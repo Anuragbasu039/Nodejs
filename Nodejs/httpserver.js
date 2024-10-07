42873dac1d49fd3434523f29356537e98a27f1f9
Anurag Bose <anuragbasu039@gmail.com>
2024-10-07 10:32:51 +0530

    Delete Nodejs directory

--- Nodejs/httpserver.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const myServer = http.createServer((req, res) => {
-    console.log("New req recived");
-    res.end("Hello from server");
-
-})
-myServer.listen(3000, () => console.log("Server started")
-);
--- Nodejs/index.js
+++
@@ -1,5 +0,0 @@
-// console.log("Hello World")
-const math = require('./modules')// import module by require function
-console.log(math.add(2,6));
-console.log(math.subtract(2,6));
-
